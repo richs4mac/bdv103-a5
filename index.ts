313a1ca5dc23c0da0d59f690cf1335503df06eba
richs4mac <210944006+richs4mac@users.noreply.github.com>
2025-07-31 04:13:05 +0000

    add watch command for tsoa

--- index.ts
+++ index.ts
@@ -1,25 +1,25 @@
-import Koa from 'koa'
-import cors from '@koa/cors'
-import zodRouter from 'koa-zod-router'
-import qs from 'koa-qs'
-import { setupBookRoutes } from './src/books'
-import { setupWarehouseRoutes } from './src/warehouse'
+import Koa from 'koa';
+import cors from '@koa/cors';
+import zodRouter from 'koa-zod-router';
+import qs from 'koa-qs';
+import { setupBookRoutes } from './src/books';
+import { setupWarehouseRoutes } from './src/warehouse';
 
-const app = new Koa()
+const app = new Koa();
 
 // We use koa-qs to enable parsing complex query strings, like our filters.
-qs(app)
+qs(app);
 
 // And we add cors to ensure we can access our API from the mcmasterful-books website
-app.use(cors())
+app.use(cors());
 
-const router = zodRouter({ zodRouter: { exposeRequestErrors: true } })
+const router = zodRouter({ zodRouter: { exposeRequestErrors: true } });
 
-setupBookRoutes(router)
-setupWarehouseRoutes(router)
+setupBookRoutes(router);
+setupWarehouseRoutes(router);
 
-app.use(router.routes())
+app.use(router.routes());
 
 app.listen(3000, () => {
-  console.log('listening!')
-})
+  console.log('listening!');
+});
