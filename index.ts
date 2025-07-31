7073b83f6c7756abdb64854b8f5f100e4c8428be
richs4mac <210944006+richs4mac@users.noreply.github.com>
2025-07-31 04:37:09 +0000

    add hello example controller & swagger

--- index.ts
+++ index.ts
@@ -1,25 +1,37 @@
+// from this example https://github.com/lukeautry/tsoa/blob/master/tests/fixtures/koa/server.ts
+
+import KoaRouter from '@koa/router';
 import Koa from 'koa';
-import cors from '@koa/cors';
-import zodRouter from 'koa-zod-router';
-import qs from 'koa-qs';
-import { setupBookRoutes } from './src/books';
-import { setupWarehouseRoutes } from './src/warehouse';
+import { koaSwagger } from 'koa2-swagger-ui';
+import swagger from './tsoa/swagger.json';
+
+import bodyParser from 'koa-bodyparser';
+import { RegisterRoutes } from './tsoa/routes';
 
 const app = new Koa();
+app.use(bodyParser());
 
-// We use koa-qs to enable parsing complex query strings, like our filters.
-qs(app);
+const router = new KoaRouter();
 
-// And we add cors to ensure we can access our API from the mcmasterful-books website
-app.use(cors());
+(RegisterRoutes as (router: KoaRouter) => void)(router);
 
-const router = zodRouter({ zodRouter: { exposeRequestErrors: true } });
-
-setupBookRoutes(router);
-setupWarehouseRoutes(router);
-
-app.use(router.routes());
-
-app.listen(3000, () => {
-  console.log('listening!');
+// It's important that this come after the main routes are registered
+app.use(async (context, next) => {
+  try {
+    await next();
+  } catch (err: any) {
+    context.status = err.status || 500;
+    context.body = err.message || 'An error occurred during the request.';
+  }
 });
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.use(koaSwagger({
+  routePrefix: '/docs',
+  specPrefix: '/docs/spec',
+  exposeSpec: true,
+  swaggerOptions: { spec: swagger }
+}));
+
+export const server = app.listen(3000);
---
+++ src/hello/helloController.ts
@@ -0,0 +1,17 @@
+// src/users/usersController.ts
+import {
+  Controller,
+  Get,
+  Path,
+  Route
+} from "tsoa";
+
+@Route("hello")
+export class HelloController extends Controller {
+  @Get("{name}")
+  public getHello(
+    @Path() name: string
+  ): string {
+    return `Hello ${name}`;
+  }
+}
