092735b2cc32660c54badc17794ca906d7afdc5a
richs4mac <210944006+richs4mac@users.noreply.github.com>
2025-08-02 16:48:10 +0000

    formatting

--- index.ts
+++ index.ts
@@ -1,39 +1,42 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+// errors are "any", this eslint config is too strict imo
+
 // from this example https://github.com/lukeautry/tsoa/blob/master/tests/fixtures/koa/server.ts
 
-import KoaRouter from '@koa/router';
-import Koa from 'koa';
-import { koaSwagger } from 'koa2-swagger-ui';
-import swagger from './tsoa/swagger.json';
+import KoaRouter from '@koa/router'
+import Koa from 'koa'
+import { koaSwagger } from 'koa2-swagger-ui'
+import swagger from './tsoa/swagger.json'
 
-import bodyParser from 'koa-bodyparser';
-import { RegisterRoutes } from './tsoa/routes';
+import bodyParser from 'koa-bodyparser'
+import { RegisterRoutes } from './tsoa/routes'
 
-const app = new Koa();
-app.use(bodyParser());
+const app = new Koa()
+app.use(bodyParser())
 
 const router = new KoaRouter();
 
-(RegisterRoutes as (router: KoaRouter) => void)(router);
+(RegisterRoutes as (router: KoaRouter) => void)(router)
 
 // It's important that this come after the main routes are registered
 app.use(async (context, next) => {
   try {
-    await next();
+    await next()
   } catch (err: any) {
-    context.status = err.status || 500;
-    context.body = err.message || 'An error occurred during the request.';
+    context.status = err?.status || 500
+    context.body = err?.message || 'An error occurred during the request.'
   }
-});
+})
 
-app.use(router.routes()).use(router.allowedMethods());
+app.use(router.routes()).use(router.allowedMethods())
 
 app.use(koaSwagger({
   routePrefix: '/docs',
   specPrefix: '/docs/spec',
   exposeSpec: true,
   swaggerOptions: { spec: swagger }
-}));
+}))
 
-export const server = app.listen(3000);
+export const server = app.listen(3000)
 
-console.log("test");
+console.log('test')
